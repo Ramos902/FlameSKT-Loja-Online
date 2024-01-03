c8ceccc2d6fe8dd65b05f3d331fdc139853e0fa6
Ramos902 <joaopedroramos1512@gmail.com>
2024-01-02 22:16:29 -0300

    Adicionando media queries no header e footer

--- FlameSKT/Assets/header.php
+++ FlameSKT/Assets/header.php
@@ -6,43 +6,46 @@
     </div>
     <div id="DivPesquisa">
         <label for="BarraPesquisa"></label>
-        <form action="index.php?src=<?php if(isset($_GET['search'])){echo($_GET['search']);}else{}?>" method="get">
+        <form action="index.php?src=<?php if (isset($_GET['search'])) {
+                                        echo ($_GET['search']);
+                                    } else {
+                                    } ?>" method="get">
             <input type="text" id="Pesquisa" name="search" placeholder="Pesquise em Todo o Site">
         </form>
     </div>
     <div id="BoxDireita">
         <div class="DivItensDireita" id="carrinho">
             <a href="./carrinho.php">
-                <div class="divImgMenuDireita">
-                    <img class="imgMenuDireita" src=".\img\site\Carrinho.png" alt="Carrinho">
-                </div>
+                <span class="material-icons md-48">
+                    local_grocery_store
+                </span>
                 <span id="textoCarrinho">Carrinho</span>
             </a>
         </div>
-        <?php if(isset($_SESSION['logado'])):?>
+        <?php if (isset($_SESSION['logado'])) : ?>
             <div class="DivItensDireita" id="minhaAcc">
                 <?php
-                    //Select Usuario
-                    $selectUsuario = "SELECT idUsuario FROM usuario where idUsuario = '$_SESSION[id_usuario]';";
-                    $execSelectUsuario = mysqli_query($conexao, $selectUsuario);
-                    $dadosUsuario = mysqli_fetch_assoc($execSelectUsuario);
+                //Select Usuario
+                $selectUsuario = "SELECT idUsuario FROM usuario where idUsuario = '$_SESSION[id_usuario]';";
+                $execSelectUsuario = mysqli_query($conexao, $selectUsuario);
+                $dadosUsuario = mysqli_fetch_assoc($execSelectUsuario);
                 ?>
                 <a href="./contaUsuario.php?">
-                    <div class="divImgMenuDireita">
-                        <img class="imgMenuDireita" src=".\img\site\Usuario.png" alt="Insta">
-                    </div>
+                    <span class="material-icons md-48">
+                        account_circle
+                    </span>
                     <span id="textoUsuario">Minha Conta</span>
                 </a>
             </div>
-        <?php else:?>
+        <?php else : ?>
             <div class="DivItensDireita" id="usuario">
                 <a href="./registro_login.php">
-                    <div class="divImgMenuDireita">
-                        <img class="imgMenuDireita" src=".\img\site\Usuario.png" alt="Insta">
-                    </div>
+                    <span class="material-icons md-48">
+                        account_circle
+                    </span>
                     <span id="textoUsuario">Faça seu Login<br> ou Cadastre-se</span>
                 </a>
-            </div>    
-        <?php endif;?>
+            </div>
+        <?php endif; ?>
     </div>
 </header>
--- FlameSKT/index.php
+++ FlameSKT/index.php
@@ -10,18 +10,19 @@ $selectProduto = "Select * from produto";
 $comandoDadosProduto = mysqli_query($conexao, $selectProduto) or die(mysqli_error($conexao));
 
 //Search Box
-if(isset($_GET['search'])){
+if (isset($_GET['search'])) {
     $pesquisa = $_GET['search'];
     $selectProduto = "SELECT * FROM produto WHERE titulo LIKE '%$pesquisa%'";
     $comandoDadosProduto = mysqli_query($conexao, $selectProduto) or die(mysqli_error($conexao));
-}else{}
+} else {
+}
 
 //Selecionar Categoria
-if(isset($_GET['cat'])){
+if (isset($_GET['cat'])) {
     $categoria = $_GET['cat'];
     switch ($categoria) {
         case 'Skate':
-            $comandoDadosProduto = mysqli_query($conexao, "SELECT * FROM produto WHERE categoria LIKE 'Skate'") or die(mysqli_error($conexao));       
+            $comandoDadosProduto = mysqli_query($conexao, "SELECT * FROM produto WHERE categoria LIKE 'Skate'") or die(mysqli_error($conexao));
             break;
         case 'LongBoard':
             $comandoDadosProduto = mysqli_query($conexao, "SELECT * FROM produto WHERE categoria LIKE 'LongBoard'") or die(mysqli_error($conexao));
@@ -36,56 +37,60 @@ if(isset($_GET['cat'])){
             $comandoDadosProduto = mysqli_query($conexao, "SELECT * FROM produto WHERE categoria LIKE 'Acessorios'") or die(mysqli_error($conexao));
             break;
         default:
-        break; 
-        }
-}else{}
+            break;
+    }
+} else {
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <link href="style.css" rel="stylesheet">
     <link rel="icon" href="img\site\icon.png">
     <link href="fullsite.css" rel="stylesheet">
     <link href="header_footer.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>FlameSKT</title>
 </head>
 
 <body>
-<?php
-require "Assets/header.php";
-require "Assets/categoria.php";
-?>
+    <?php
+    require "Assets/header.php";
+    require "Assets/categoria.php";
+    ?>
     <main id="main">
         <div id="div-conteudo">
             <?php
-            while ($Produtos = mysqli_fetch_assoc($comandoDadosProduto)) : 
-                if($Produtos['status'] == "ativo"):
+            while ($Produtos = mysqli_fetch_assoc($comandoDadosProduto)) :
+                if ($Produtos['status'] == "ativo") :
             ?>
-                <div class="div-produtos">
-                    <a href="produto.php?id=<?=$Produtos['idProduto']?>" class="produtos-linha">
-                        <div class="ImagemProduto">
-                            <img src="<?=$Produtos['img']?>" width="200" height="auto" alt="ImagemProduto">
-                        </div>
-                        <div class="NomeProduto">
-                            <p><?=$Produtos['titulo']?></p>
-                        </div>
-                        <div class="PrecoProduto">
-                            <span class="preco">R$<?= number_format((float)$Produtos['preco'], 2, ',', '') ?><br></span>
-                            <span class="precoX">Ou <?= $Produtos['qntVezes'] ?>x <?= number_format(((float)$Produtos['preco'] / (int)$Produtos['qntVezes']), 2, ',', '') ?><br></span>
-                            <span class="frete">+Frete Grátis</span>
-                        </div>
-                    </a>
-                </div>
-            <?php else: endif;
-                endwhile;
+                    <div class="div-produtos">
+                        <a href="produto.php?id=<?= $Produtos['idProduto'] ?>" class="produtos-linha">
+                            <div class="ImagemProduto">
+                                <img src="<?= $Produtos['img'] ?>" width="200" height="auto" alt="ImagemProduto">
+                            </div>
+                            <div class="NomeProduto">
+                                <p><?= $Produtos['titulo'] ?></p>
+                            </div>
+                            <div class="PrecoProduto">
+                                <span class="preco">R$<?= number_format((float)$Produtos['preco'], 2, ',', '') ?><br></span>
+                                <span class="precoX">Ou <?= $Produtos['qntVezes'] ?>x <?= number_format(((float)$Produtos['preco'] / (int)$Produtos['qntVezes']), 2, ',', '') ?><br></span>
+                                <span class="frete">+Frete Grátis</span>
+                            </div>
+                        </a>
+                    </div>
+            <?php else : endif;
+            endwhile;
             ?>
         </div>
     </main>
-<?php
+    <?php
     require "Assets/footer.php";
-?>
+    ?>
 </body>
+
 </html>
